9ee0ad4d72edfc6936e8404e42ec31c42673b504
Mock1ng <bayuw1123@gmail.com>
2020-09-04 09:59:33 +0700

    Updates

---
+++ precache-manifest.c5217d82ba00c0cf12973998b7d884dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38865a5c8819e8d34e8c07e37379225e",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/index.html"
+  },
+  {
+    "revision": "580751e729ba8e014f6d",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/static/css/main.72f85b9d.chunk.css"
+  },
+  {
+    "revision": "78c4eaf96680cad5e6c6",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/static/js/2.75951975.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/static/js/2.75951975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580751e729ba8e014f6d",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/static/js/main.9c6fa237.chunk.js"
+  },
+  {
+    "revision": "baede669572e25d74cc8",
+    "url": "/Mock1ng.github.io/uflix-movie-details-provider/static/js/runtime-main.28f28465.js"
+  }
+]);
