85f3c8521c5ac90a26454fafcda072393ef26fdd
Mock1ng <bayuw1123@gmail.com>
2020-09-04 10:07:15 +0700

    Updates

---
+++ precache-manifest.f1fc4af2f9dee5cb6cac5efd7944b2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3af054b8f0c056c0b355275a4fdae6",
+    "url": "/uflix-movie-details-provider/index.html"
+  },
+  {
+    "revision": "1a95ded9e283d9342db7",
+    "url": "/uflix-movie-details-provider/static/css/main.72f85b9d.chunk.css"
+  },
+  {
+    "revision": "78c4eaf96680cad5e6c6",
+    "url": "/uflix-movie-details-provider/static/js/2.75951975.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uflix-movie-details-provider/static/js/2.75951975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a95ded9e283d9342db7",
+    "url": "/uflix-movie-details-provider/static/js/main.bf7e9ae1.chunk.js"
+  },
+  {
+    "revision": "4ff5074d112f2bfd7860",
+    "url": "/uflix-movie-details-provider/static/js/runtime-main.fcce0356.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mock1ng.github.io/uflix-movie-details-provider/precache-manifest.c5217d82ba00c0cf12973998b7d884dc.js"
+  "/uflix-movie-details-provider/precache-manifest.f1fc4af2f9dee5cb6cac5efd7944b2fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mock1ng.github.io/uflix-movie-details-provider/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uflix-movie-details-provider/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
